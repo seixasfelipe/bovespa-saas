bae026fa44ff4f759083b1ba1d862ea9da2af98d
Felipe Seixas <seixasfelipe@gmail.com>
2013-06-05 08:21:41 -0400

    simple tests

--- spec/bovespa_saas/app_spec.rb
+++ spec/bovespa_saas/app_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
-describe 'The HelloWorld App' do
+describe 'Bovespa SaaS App' do
 
   it "works for me" do
     get '/'
     last_response.should be_ok
     last_response.body.should == "It works!"
+  end
+
+  it "should gets one day stock quote prices" do
+    get '/stock/VALE3/2003/02/12'
+    last_response.should be_ok
+    last_response.body.should == '{"ticker_symbol":"VALE3","opening_price":"105.01"}'
+  end
+
+  it "should gets 404 when stock (ticker symbol) doesnt exists" do
+    get '/stock/foradun/2000/01/01'
+    last_response.should be_not_found
+  end
+
+  it "should gets 404 when stock date doesnt exists" do
+    get '/stock/VALE3/2004/12/25'
+    last_response.should be_not_found
   end
 
 end
